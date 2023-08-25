95475b9ee254904e3f5f0a577e6388a5305a763c
NeilRow56 <neilrowland56@gmail.com>
2023-08-25 06:15:35 +0100

    first commit- folder setup - shadcn added

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+    <p className="text-6xl text-green-600">Dashboard Page (Protected)</p>
+    <Button>Click me</Button>
+    </div>
+    
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='p-3'>
+      Landing Page (Unprotected)
+    </div>
+  )
+}
+
+export default LandingPage
