762e2c7506243bb7a70022cfaa1d04d09d133c98
NeilRow56 <neilrowland56@gmail.com>
2023-08-28 07:08:40 +0100

    Image generation awaiting openai v4

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+
 import Sidebar from '@/components/Sidebar'
 
 export default function DashboardLayout({
@@ -11,9 +12,10 @@ export default function DashboardLayout({
       <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-blue-800 md:w-72">
         <Sidebar />
       </div>
-      <main className="md:pl-72 ">
+      <main className="md:pl-72 bg-green-100 h-full ">
         <Navbar />
         {children}
+        
       </main>
     </div>
   )
